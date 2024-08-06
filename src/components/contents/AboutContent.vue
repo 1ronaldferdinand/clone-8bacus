4bccb0a4dd14e5ae11090982f45e1bb265d21d57
Ronald Ferdinand <ronald@gamatechno.com>
2024-08-06 13:56:56 +0700

    about content

--- src/components/contents/AboutContent.vue
+++ src/components/contents/AboutContent.vue
@@ -24,7 +24,7 @@
             </span>
             <div class="line-spacer w-100"></div>
         </div>
-        <div class="d-flex align-items-start justify-content-center" style="padding: 10px 0px;">
+        <div class="d-flex flex-column flex-md-row align-items-center align-items-md-start justify-content-start justify-content-md-center" style="padding: 10px 0px;">
             <div class="my-card">
                 <img height="172" src="@/assets/icons/8b-icon-0005.png" alt="">													
                 <span class="my-card-title">Industry Expertise</span>
@@ -55,7 +55,7 @@
                 </p>
             </div>
         </div>
-        <div class="d-flex align-items-start justify-content-center" style="padding: 10px 0px;">
+        <div class="d-flex flex-column flex-md-row align-items-center align-items-md-start justify-content-start justify-content-md-center" style="padding: 10px 0px;">
             <div class="my-card">
                 <img height="172" src="@/assets/icons/8b-icon-0008.png" alt="">													
                 <span class="my-card-title">Transparent Processes</span>
@@ -77,9 +77,41 @@
                 </p>
             </div>
         </div>
-        <div class="other-services d-flex flex-column-reverse flex-md-row" style="padding: 0px 80px;">
-            <div class="col-12 col-md-10"></div>
-            <div class="col-12 col-md-2" style="text-align: end;">
+        <div class="other-services d-flex flex-column-reverse flex-lg-row">
+            <div class="col-12 col-md-10 section-container">
+                <div class="section step-1">
+                    <span style="font-size: 18px; font-weight: 600; line-height: 18px;">
+                        Strategic Recruitment
+                    </span>
+                    <p style="font-size: 12px; font-weight: 400; line-height: 20px;">
+                        Elevate your workforce with our strategic recruitment solutions. 
+                        We go beyond just filling positions 
+                        – we find the perfect match for your company culture and objectives, 
+                        ensuring long-term success.
+                    </p>
+                </div>
+                <div class="section step-2">
+                    <span style="font-size: 18px; font-weight: 600; line-height: 18px;">
+                        Effortless Administration
+                    </span>
+                    <p style="font-size: 12px; font-weight: 400; line-height: 20px;">
+                        Say goodbye to administrative hassles. 
+                        Our comprehensive admin services cover everything from documentation management to HR support, 
+                        allowing you to focus on your core business functions.
+                    </p>
+                </div>
+                <div class="section step-3">
+                    <span style="font-size: 18px; font-weight: 600; line-height: 18px;">
+                        Visa Expertise
+                    </span>
+                    <p style="font-size: 12px; font-weight: 400; line-height: 20px;">
+                        Navigating visa processes can be a labyrinth. 
+                        Let our visa experts guide you through the complexities, 
+                        offering reliable and timely services for individuals and businesses alike.
+                    </p>
+                </div>
+            </div>
+            <div class="col-12 d-block d-lg-none" style="text-align: center; line-height: 24px;">
                 Unlock Your
                 Business
                 Potential with
@@ -90,6 +122,18 @@
                 Complete
                 Solutions
                 Partner!
+            </div>
+            <div class="col-2 d-none d-lg-block" style="text-align: end; line-height: 42px;">
+                Unlock Your<br>
+                Business<br>
+                Potential with<br>
+                <span style="color:#F37721; font-weight:600;">
+                    8Bacus Corp.<br>
+                </span>
+                Your<br>
+                Complete<br>
+                Solutions<br>
+                Partner!<br>
             </div>
         </div>
     </div>
@@ -191,11 +235,50 @@ export default {}
 }
 
 .other-services {
-    padding-top: 100px;
     color: #000000;
     font-family: "Roboto", Sans-serif;
-    font-size: 28px;
+    font-size: 20px;
     font-weight: 400;
     line-height: 46px;
+    padding: 100px 120px;
+}
+
+.section-container {
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+}
+
+.section {
+    max-width: 480px;
+    text-align: start;
+    position: relative;
+}
+
+.section.step-1 {
+    left: 0;
+}
+
+.section.step-2 {
+    left: 90px; /* Adjust as needed */
+}
+
+.section.step-3 {
+    left: 180px; /* Adjust as needed */
+}
+
+@media (max-width: 992px){
+    .other-services {
+        padding: 100px 80px;
+        gap: 20px
+    }
+
+    .section.step-2 {
+        left: 0; /* Adjust as needed */
+    }
+
+    .section.step-3 {
+        left: 0; /* Adjust as needed */
+    }
 }
 </style>
